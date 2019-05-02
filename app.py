817f0097d29b460dbf23d800ac9f9c094ddda40e
stuffofminsun <metokki@gmail.com>
2019-05-02 15:16:34 -0400

    completed second chart, added a bunch of stuff to get heroku going

--- app.py
+++ app.py
@@ -4,24 +4,36 @@ import dash_html_components as html
 from dash.dependencies import Input, Output, State
 import plotly.graph_objs as go
 import pandas as pd
+import numpy as np
 
-# data
+# data for trading volumes
 df = pd.read_csv('agency.csv', header=0)
 df.replace('*', '', inplace=True)
-
 blah = df.loc[:, ((df.columns != 'Date') ^ (df.columns != 'AssetClass') ^ (df.columns !='AssetClassSubType'))].apply(pd.to_numeric)
 df = pd.concat((df['AssetClass'], df['AssetClassSubType'], blah, df['Date']), axis=1)
 agency_trades = df.groupby('Date').sum()
 
+# data for prices
+df2 = pd.read_csv('tbaprices.csv', header=0)
+# get rid of stars, get rid of 0s
+df2.replace('*', '', inplace=True)
+df2.replace('0', '', inplace=True)
+df2['Date'] = pd.to_datetime(df2['Date'])
+
+# convert coupons to numbs
+coupons = ['<=2.5', '3', '3.5', '4', '4.5', '5', '>5.0']
+df2[coupons] = df2[coupons].apply(pd.to_numeric)
+df2 = df2.replace(np.NaN, '')
+
 # launch app
 app = dash.Dash(__name__)
-app.title = 'Agency and Non-Agency Trading'
+app.title = 'Agency Trading Volumes and Prices (FINRA)'
 server = app.server
 
 #build and serve layouts
 def serve_layout():
     return html.Div(children=[
-        html.H5(children='Agency and Non-Agency Trading Volumes (FINRA)'),
+        html.H5(children='Agency Trading Volumes (FINRA)'),
         html.Div([
             html.Div([
                 dcc.Dropdown(options=[
@@ -33,29 +45,80 @@ def serve_layout():
                     value='trades_num'),], className='one-third column'),
             html.Div([
                 dcc.Dropdown(options=[
-                    {'label': 'Total', 'value': 'TOTAL'},
-                    {'label': 'Agency Pass-Through (TBA, STIP, & Rolls)', 'value': 'AGENCY PASS-THRU (TBA, STIP, $ ROLLS)'},
-                    {'label': 'Agency Pass-Through (Specified)', 'value': 'AGENCY PASS-THRU (SPECIFIED)'},
-                    {'label': 'Agency CMO', 'value': 'AGENCY CMO'}],
+                    {'label': 'Trade Total', 'value': 'TOTAL'},
+                    {'label': 'Trade Type: Agency Pass-Through (TBA, STIP, & Rolls)', 'value': 'AGENCY PASS-THRU (TBA, STIP, $ ROLLS)'},
+                    {'label': 'Trade Type: Agency Pass-Through (Specified)', 'value': 'AGENCY PASS-THRU (SPECIFIED)'},
+                    {'label': 'Trade Type: Agency CMO', 'value': 'AGENCY CMO'}],
                     placeholder='Select Trade Type',
                     id='security type',
                     value='TOTAL'),], className='one-third column'),
             html.Div([
                 dcc.Dropdown(options=[
-                    {'label': 'Total', 'value': 'TOTAL'},
-                    {'label': 'Single Family 15Y', 'value': 'SINGLE FAMILY 15Y'},
-                    {'label': 'Single Family 30Y', 'value': 'SINGLE FAMILY 30Y'},
-                    {'label': 'Other', 'value': 'OTHER'},
-                    {'label': 'Adjustable/Hybrid', 'value': 'ADJUSTABLE/HYBRID'},
-                    {'label': 'P&I', 'value': 'P&I'},
-                    {'label': 'IO/PO', 'value': 'IO/PO'}],
+                    {'label': 'Total Mortgages', 'value': 'TOTAL'},
+                    {'label': 'Mortgage Type: Single Family 15Y', 'value': 'SINGLE FAMILY 15Y'},
+                    {'label': 'Mortgage Type: Single Family 30Y', 'value': 'SINGLE FAMILY 30Y'},
+                    {'label': 'Mortgage Type: Other', 'value': 'OTHER'},
+                    {'label': 'Mortgage Type: Adjustable/Hybrid', 'value': 'ADJUSTABLE/HYBRID'},
+                    {'label': 'Mortgage Type: P&I', 'value': 'P&I'},
+                    {'label': 'Mortgage Type: IO/PO', 'value': 'IO/PO'}],
                     placeholder='Select Trade Type',
                     id='mortgage type',
                     value='TOTAL'),], className='one-third column'),
         ], className='row'),
-
-
         dcc.Graph(id='agency trades'),
+
+        # now start the second pricing chart
+        html.H5(children='Agency TBA Prices (FINRA)'),
+        html.Div([
+            html.Div([
+                dcc.Dropdown(options=[
+                    {'label': 'Average Price', 'value': 'AVERAGE PRICE'},
+                    {'label': 'Weighted Average Price', 'value': 'WEIGHTED AVG. PRICE'},
+                    {'label': 'Average Price, Bottom 5 Trades', 'value': 'AVG. PRICE BOTTOM 5 TRADES'},
+                    {'label': 'Average Price, Top 5 Trades', 'value': 'AVG. PRICE TOP 5 TRADES'},
+                    {'label': '2nd Quartile Price', 'value': '2ND QUARTILE PRICE'},
+                    {'label': '3rd Quartile Price', 'value': '3RD QUARTILE PRICE'},
+                    {'label': '4th Quartile Price', 'value': '4TH QUARTILE PRICE'},
+                    {'label': 'Standard Deviation', 'value': 'STANDARD DEVIATION'},
+                    {'label': "Volume of Trades (Thousands)", 'value': "VOLUME OF TRADES (000'S)"},
+                    {'label': 'Number of Trades', 'value': 'NUMBER OF TRADES'}],
+                    placeholder='Select Measure',
+                    id='select measure',
+                    value='AVERAGE PRICE'),
+            ], className='three columns'),
+            html.Div([
+                dcc.Dropdown(options=[
+                    {'label': 'Mortgage: Single Family 15Y', 'value': 'SINGLE FAMILY 15Y'},
+                    {'label': 'Mortgage: Single Family 30Y', 'value': 'SINGLE FAMILY 30Y'},],
+                    placeholder='Select Asset Subtype',
+                    id='select asset class subtype',
+                    value='SINGLE FAMILY 15Y'),
+            ], className='three columns'),
+            html.Div([
+                dcc.Dropdown(options=[
+                    {'label': 'Coupon: <=2.5', 'value': '<=2.5'},
+                    {'label': 'Coupon: 3', 'value': '3'},
+                    {'label': 'Coupon: 3.5', 'value': '3.5'},
+                    {'label': 'Coupon: 4', 'value': '4'},
+                    {'label': 'Coupon: 4.5', 'value': '4.5'},
+                    {'label': 'Coupon: 5.0', 'value': '5'},
+                    {'label': 'Coupon: >5.0', 'value': '>5.0'}, ],
+                    placeholder='Select Asset Subtype',
+                    id='select coupon type',
+                    value='3.5'),
+            ], className='three columns'),
+            html.Div([
+                dcc.Dropdown(options=[
+                    {'label': 'Settlement Date: Current Month', 'value': 'Current Month'},
+                    {'label': 'Settlement Date: Current Month + 1', 'value': 'Current Month + 1'},
+                    {'label': 'Settlement Date: Current Month + 2', 'value': 'Current Month + 2'},
+                    {'label': 'Settlement Date: Current Month + 3', 'value': 'Current Month + 3'}, ],
+                    placeholder='Select Settlement Month',
+                    id='select settlement month',
+                    value='Current Month'),
+            ], className='three columns'),
+        ], className='row'),
+        dcc.Graph(id='agency prices'),
     ])
 app.layout = serve_layout
 
@@ -99,28 +162,73 @@ def build_figure(tradetype, securitytype, mortgagetype,):
                     x=agency_trades.index,
                     y=tempdf[temp_columns[0]].values,
                     name='FNMA',
-                    hoverinfo='skip',
                     orientation='v'
                 ),
                 go.Scatter(
                     x=agency_trades.index,
                     y=tempdf[temp_columns[1]].values,
                     name='FHLMC',
-                    hoverinfo='skip',
                     orientation='v'
                 ),
                 go.Scatter(
                     x=agency_trades.index,
                     y=tempdf[temp_columns[2]].values,
                     name='GNMA',
-                    hoverinfo='skip',
                     orientation='v'
                 ),
                 go.Scatter(
                     x=agency_trades.index,
                     y=tempdf[temp_columns[3]].values,
                     name='Other',
-                    hoverinfo='skip',
+                    orientation='v'
+                ),
+            ],
+            'layout': go.Layout(
+                height=600,
+                title=chart_title,
+            )
+        }
+
+
+def build_pricefigure(selected_measure, selected_asset_class_subtype, selected_coupon_type, selected_settlement_month):
+    test = df2[df2['Measure'] == 'AVERAGE PRICE']
+    test = test[test['AssetClassSubType'] == 'SINGLE FAMILY 30Y']
+    test = test[test['SettlementDateChart'] == 'Current Month']
+
+    # let's start with measure:
+    temp = df2[df2['Measure'] == selected_measure]
+    temp = temp[temp['AssetClassSubType'] == selected_asset_class_subtype]
+    temp = temp[temp['SettlementDateChart'] == selected_settlement_month]
+    temp = temp.set_index(['Date', 'Agency']).unstack(level=1)[selected_coupon_type]
+
+    # let's build title
+    chart_title = '{}, {}, {}, {}'.format(selected_measure, selected_asset_class_subtype, selected_coupon_type,
+                                          selected_settlement_month)
+
+    return {
+            'data': [
+                go.Scatter(
+                    x=temp.index,
+                    y=temp['FNMA'].values,
+                    name='FNMA',
+                    orientation='v'
+                ),
+                go.Scatter(
+                    x=temp.index,
+                    y=temp['FNMA/UMBS'].values,
+                    name='FNMA/UMBS',
+                    orientation='v'
+                ),
+                go.Scatter(
+                    x=temp.index,
+                    y=temp['FHLMC'].values,
+                    name='FHLMC',
+                    orientation='v'
+                ),
+                go.Scatter(
+                    x=temp.index,
+                    y=temp['GNMA'].index,
+                    name='GNMA',
                     orientation='v'
                 ),
             ],
@@ -137,9 +245,19 @@ def build_figure(tradetype, securitytype, mortgagetype,):
      Input('mortgage type', 'value')]
 )
 def update_figure(selected_trade, selected_security, selected_mortgage):
-    #print(selected_trade, selected_security, selected_mortgage)
     return build_figure(selected_trade, selected_security, selected_mortgage)
+
+@app.callback(
+    Output('agency prices', 'figure'),
+    [Input('select measure', 'value'),
+     Input('select asset class subtype', 'value'),
+     Input('select coupon type', 'value'),
+     Input('select settlement month', 'value'),
+     ]
+)
+def update_figure(selected_measure, selected_asset_class_subtype, selected_coupon_type, selected_settlement_month):
+    return build_pricefigure(selected_measure, selected_asset_class_subtype, selected_coupon_type, selected_settlement_month)
 
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server()
--- cleanFINRAABS.py
+++ cleanFINRAABS.py
@@ -6,65 +6,127 @@ import numpy as np
 # takes a zip list of FINRA ABS trading files, cleans them up and spits them out into 2 raw CSV files.
 directory = os.fsencode('data')
 
-for zfile in os.listdir(directory):
-    filename = os.fsdecode(zfile)
-    zf = zipfile.ZipFile('data/' + filename)
+def getTradingVolumes(directory):
+    for zfile in os.listdir(directory):
+        filename = os.fsdecode(zfile)
+        zf = zipfile.ZipFile('data/' + filename)
 
-    # building dates
-    filedate = filename.split('-')[1].split('.')[0]
+        # build namelist of specific files I need
+        star_list = [i for i in zf.namelist() if 'STAR' in i]
 
-    # build namelist of specific files I need
-    star_list = [i for i in zf.namelist() if 'STAR' in i]
+        for trading_day in star_list:
+            # get trading day date
+            trading_date = trading_day.split('-')[1].replace('.xlsx', '')
 
-    for trading_day in star_list:
-        # get trading day date
-        trading_date = trading_day.split('-')[1].replace('.xlsx', '')
+            # should i fix trading date?
+            trading_date = '{}/{}/{}'.format(trading_date[4:6], trading_date[6:], trading_date[:4])
 
-        # should i fix trading date?
-        trading_date = '{}/{}/{}'.format(trading_date[4:6], trading_date[6:], trading_date[:4])
+            # testing on a single file
+            df = pd.read_excel(zf.open(trading_day), sheet_name='TradingActivity', skiprows=11,
+                               skipfooter=8, header=None)
 
-        # testing on a single file
-        df = pd.read_excel(zf.open(trading_day), sheet_name='TradingActivity', skiprows=11,
-                           skipfooter=8, header=None)
+            # move some headers to the left, fill out NaN
+            df[0] = df[1].where(df[2].isna()).fillna(method='ffill')
 
-        # move some headers to the left, fill out NaN
-        df[0] = df[1].where(df[2].isna()).fillna(method='ffill')
+            # split into agency and non-agency
+            agency_df = df[:12].copy()
+            nonagency_df = df[16:].copy()
 
-        # split into agency and non-agency
-        agency_df = df[:12].copy()
-        nonagency_df = df[16:].copy()
+            # agency - filter out excess rows and add columns
+            agency_df = agency_df[~(agency_df[0] == agency_df[1])]
+            agency_df.columns = ['AssetClass', 'AssetClassSubType', 'FNMATradeCount', 'FNMAUniqueID', 'FNMA$Trades',
+                                 'FHLMCTradeCount', 'FHLMCUniqueID', 'FHLMC$Trades', 'GNMATradeCount', 'GNMAUniqueID',
+                                 'GNMA$Trades', 'OtherTradeCount', 'OtherUniqueID', 'Other$Trades']
 
-        # agency - filter out excess rows and add columns
-        agency_df = agency_df[~(agency_df[0] == agency_df[1])]
-        agency_df.columns = ['AssetClass', 'AssetClassSubType', 'FNMATradeCount', 'FNMAUniqueID', 'FNMA$Trades',
-                             'FHLMCTradeCount', 'FHLMCUniqueID', 'FHLMC$Trades', 'GNMATradeCount', 'GNMAUniqueID',
-                             'GNMA$Trades', 'OtherTradeCount', 'OtherUniqueID', 'Other$Trades']
+            # agency - add date
+            agency_df['Date'] = trading_date
 
-        # agency - add date 
-        agency_df['Date'] = trading_date
+            # non-agency = filter out excess columns, rename some mistakes
+            nonagency_df = nonagency_df[~(nonagency_df[0] == nonagency_df[1])].drop([8, 9, 10, 11, 12, 13], axis=1)
+            nonagency_df[0] = nonagency_df[1].where(
+                (nonagency_df[1] == 'CBO/CDO/CLO') ^ (nonagency_df[1] == 'OTHER')).fillna(nonagency_df[0])
+            nonagency_df.columns = ['AssetClass', 'AssetClass2', 'IGTradeCount', 'IGUniqueID', 'IG$Trades', 'HYTradeCount',
+                                    'HYUniqueID', 'HY$Trades']
 
-        # non-agency = filter out excess columns, rename some mistakes
-        nonagency_df = nonagency_df[~(nonagency_df[0] == nonagency_df[1])].drop([8, 9, 10, 11, 12, 13], axis=1)
-        nonagency_df[0] = nonagency_df[1].where(
-            (nonagency_df[1] == 'CBO/CDO/CLO') ^ (nonagency_df[1] == 'OTHER')).fillna(nonagency_df[0])
-        nonagency_df.columns = ['AssetClass', 'AssetClass2', 'IGTradeCount', 'IGUniqueID', 'IG$Trades', 'HYTradeCount',
-                                'HYUniqueID', 'HY$Trades']
+            # non-agency add date and set index
+            nonagency_df['Date'] = trading_date
+            # nonagency_df.set_index('AssetClass', inplace=True)
 
-        # non-agency add date and set index
-        nonagency_df['Date'] = trading_date
-        # nonagency_df.set_index('AssetClass', inplace=True)
+            # check to see if filepath exists and append headers if they don't
+            if os.path.isfile('agency.csv'):
+                pass
+            else:
+                pd.DataFrame(agency_df.columns).transpose().to_csv('agency.csv', header=False, index=False, mode='a')
 
-        # check to see if filepath exists and append headers if they don't
-        if os.path.isfile('agency.csv'):
-            pass
-        else:
-            pd.DataFrame(agency_df.columns).transpose().to_csv('agency.csv', header=False, index=False, mode='a')
+            if os.path.isfile('nonagency.csv'):
+                pass
+            else:
+                pd.DataFrame(nonagency_df.columns).transpose().to_csv('nonagency.csv', header=False, index=False, mode='a')
 
-        if os.path.isfile('nonagency.csv'):
-            pass
-        else:
-            pd.DataFrame(nonagency_df.columns).transpose().to_csv('nonagency.csv', header=False, index=False, mode='a')
+                # otherwise, append the file without  headers
+            agency_df.to_csv('agency.csv', header=False, index=False, mode='a')
+            nonagency_df.to_csv('nonagency.csv', header=False, index=False, mode='a')
 
-            # otherwise, append the file without  headers
-        agency_df.to_csv('agency.csv', header=False, index=False, mode='a')
-        nonagency_df.to_csv('nonagency.csv', header=False, index=False, mode='a')
+def gettbaprices(directory):
+    for zfile in os.listdir(directory):
+        filename = os.fsdecode(zfile)
+        zf = zipfile.ZipFile('data/' + filename)
+
+        month_list = {'January': 1, 'February': 2, 'March': 3, 'April': 4, 'May': 5, 'June': 6,
+                      'July': 7, 'August': 8, 'September': 9, 'October': 10, 'November': 11, 'December': 12}
+
+        price_fields = ['AVERAGE PRICE', 'WEIGHTED AVG. PRICE', 'AVG. PRICE BOTTOM 5 TRADES', '2ND QUARTILE PRICE',
+                        '3RD QUARTILE PRICE', '4TH QUARTILE PRICE', 'AVG. PRICE TOP 5 TRADES', 'STANDARD DEVIATION',
+                        'VOLUME OF TRADES (000\'S)', 'NUMBER OF TRADES']
+
+        # build namelist of specific files I need
+        pxtables_list = [i for i in zf.namelist() if 'PXTABLES' in i]
+
+        for trading_day in pxtables_list:
+            # get trading day date
+            trading_date = trading_day.split('-')[1].replace('.xlsx', '')
+
+            # should i fix trading date?
+            trading_date = '{}/{}/{}'.format(trading_date[4:6], trading_date[6:], trading_date[:4])
+
+            # build monthlist:
+            current_month = int(trading_date[0:2])
+            chart_month_list = {current_month: 'Current Month', (current_month + 1) % 12: 'Current Month + 1',
+                                (current_month + 2) % 12: 'Current Month + 2',
+                                (current_month + 3) % 12: 'Current Month + 3'}
+
+            # start reading stuff in
+            df = pd.read_excel(zf.open(trading_day), sheet_name='TBA', skiprows=8, header=None)
+            df[0] = df[1].where(~df[1].isin(price_fields)).fillna(method='ffill')
+
+            # let's slice off the bottom part of this based on when the footnotes start - this is a variable line sheet
+            df = df.loc[:df[df[0].str.contains('Indicates')].index.values[0] - 2]
+
+            # let's pull out the settlement dates
+            df[9] = df[0].where(df[0].str.contains('Settlement')).fillna(method='ffill')
+
+            df[10] = df[0].where(df[0].str.contains('PRICING TABLE')).fillna(method='ffill')
+            new = df[10].str.split(' - ', expand=True)
+            df[11] = new[0]
+            df[12] = new[1]
+            df.drop(10, axis=1, inplace=True)
+            df[11] = df[11].str.replace('PRICING TABLE: ', '')
+            df = df[~(df[0] == df[1])]
+            df.columns = ['Agency', 'Measure', '<=2.5', '3', '3.5', '4', '4.5', '5', '>5.0', 'SettlementMonth',
+                          'AssetClassType', 'AssetClassSubType']
+            df['SettlementMonth'] = df['SettlementMonth'].str.replace(' Settlement', '')
+            df['Date'] = trading_date
+            df['SettlementDateChart'] = [chart_month_list[month_list[d]] for d in df['SettlementMonth']]
+
+            # clean up some null stuff
+            df = df[~(df['Measure'].isna())]
+
+            # check to see if filepath exists and append headers if they don't
+            if os.path.isfile('tbaprices.csv'):
+                pass
+            else:
+                pd.DataFrame(df.columns).transpose().to_csv('tbaprices.csv', header=False, index=False, mode='a')
+
+            df.to_csv('tbaprices.csv', header=False, index=False, mode='a')
+
+gettbaprices(directory)
